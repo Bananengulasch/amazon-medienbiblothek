9596fe93a6e889620cf8a715927f85bd46665c55
Alex <Alex@Alexander-PC>
2018-10-25 14:09:56 +0200

    Medienbibliothek

--- amazon-medienbibliothek/src/Medienbibliothek.java
+++ amazon-medienbibliothek/src/Medienbibliothek.java
@@ -3,4 +3,15 @@ import java.util.ArrayList;
 public class Medienbibliothek {
 	
 	ArrayList<Medien> mediumsliste = new ArrayList<Medien>();
+
+public void medienHinzufuegen(Medien m) {
+	mediumsliste.add(m);
+}
+
+public void anzeigen() {
+	for(Medien m : mediumsliste) {
+		m.anzeigen();
+	}
+}
+
 }
